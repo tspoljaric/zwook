8c4c4576cab3b927ff40c0dd2327bd085dae270b
tspoljaric <tspoljaric07@gmail.com>
2025-09-01 18:21:25 +0200

    Register page first version

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
 export class Register {
+  name: string = '';
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  passwordConfirmation: string = '';
+
+  errorMessage: string | null = null;
+  isLoading: boolean = false;
+
+  constructor(private router: Router) {}
+
+  onLogin() {
+    this.router.navigate(['/dashboard']);
+  }
 
 }
