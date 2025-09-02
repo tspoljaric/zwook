87fcb09c569f0d6cb59734bacfd3b1effe2d25d7
tspoljaric <tspoljaric07@gmail.com>
2025-09-02 18:12:37 +0200

    Sidebar first version

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Sidebar } from "../../layout/sidebar/sidebar";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [Sidebar],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss'
 })
 export class Sidebar {
+  showCard: boolean = true;
 
+  dismissCard() {
+    this.showCard = false;
+  
+  }
 }
