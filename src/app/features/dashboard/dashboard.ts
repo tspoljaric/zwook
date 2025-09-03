bee97739dc41355702ebf0c56af28c0ab876a871
tspoljaric <tspoljaric07@gmail.com>
2025-09-03 14:55:15 +0200

    Navbar first version

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../../layout/sidebar/sidebar";
+import { Navbar } from "../../layout/navbar/navbar";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Sidebar],
+  imports: [Sidebar, Navbar],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
